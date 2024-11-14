a2385f2fa521d3389135f4ce05c4557eab9ec80d
花裤衩 <casey@movthing.com>
2024-11-14 21:35:06 +0800

    theme is auro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import Aura from '@primevue/themes/Aura'
+import Aura from '@primevue/themes/aura'
 
 export default defineNuxtConfig({
   modules: [
